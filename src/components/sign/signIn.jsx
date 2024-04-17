7e744bba957f0ddfecf3a857105894920a533d78
unknown <Mahmoud-Fauome>
2024-04-17 21:46:23 +0200

    my changes

--- src/components/sign/signIn.jsx
+++ src/components/sign/signIn.jsx
@@ -36,7 +36,7 @@ const SignIn = () => {
   document.body.style.backgroundColor = "#bec0c8";
   return (
     <div
-      style={{ paddingBottom: "7em", paddingTop: "7em" }}
+      style={{ paddingBottom: "7em", paddingTop: "7.3em" }}
       className="mt-1 fw-bolder d-flex flex-column align-items-center"
     >
       <form onSubmit={handelSignIn}>
